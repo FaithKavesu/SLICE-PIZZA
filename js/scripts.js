6881f7ecbd128b3493d419114a5b66feeffe18fa
Faith <kavesumulili@gmail.com>
2020-02-28 15:44:01 +0300

    Add pizza menu

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {  
+$("#img1").click(function(){
+$("#img1").slideToggle();
+$("#home").toggle();
+});
+$("#home").click(function(){
+$("#img1").slideToggle();
+$("#home").toggle();
+});
+$("#img2").click(function() {
+$("#img2").slideToggle();
+$("#about us").toggle();
+});
+$("#about us").click(function() {
+$("#img2").slideToggle();
+$("#about us").toggle();
+})
+$("#img3").click(function() {
+$("#img3").slideToggle();
+$("#menu").slideToggle();
+})
+$("#menu").click(function() {
+$("#img3").slideToggle();
+  $("#menu").slideToggle();
+})
+})
