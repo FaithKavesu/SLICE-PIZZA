53e4d5350dc2f100e7bd4302b460ae8885069ef9
Faith <kavesumulili@gmail.com>
2020-03-05 10:36:22 +0300

    Add changes to total

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,63 @@
-  function getSize(){
-    var size = document.getElementById("size").value
-    return parseInt(size);
-  }
-  function getCrust(){
-    var crust = document.getElementById("crust").value
-    return parseInt(crust);
-  }
-  function getToppings(){
-    var topping = document.getElementById("topping").value
-    return parseInt(topping);
-  }
-  function getNumber(){
-    var number = document.getElementById("number").value
-    return parseInt(number);
-  }
-  function getCost(){
-    var Totalcost =(getSize()+getCrust()+getToppings()+getNumber());
-    alert("Thank you for using SLICE PIZZA,your total amounts to" + (Totalcost));
-  };
-function validate() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    if (name === "") {
-        alert("Please write your name!");
-    } else if (email === "") {
-        alert("Please write your name!");
-    } else if (message === "") {
-        alert("Please write a message");
-    } else {
-        alert("We have recieved  your message dear " + name + ", " + " thank you for getting in touch with us");
-    };
+function Order(type, size, crust, topping) {
+  this.type = type;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
 }
+
+Order.prototype.fullOrder = function () {
+  return this.type + " with the topping of " + this.topping + " and " + this.crust + " as crust.";
+};
+
+function Total(price, quantity, delivery) {
+  this.price = price;
+  this.quantity = quantity;
+  this.delivery = delivery;
+}
+
+Total.prototype.finalTotal = function () {
+  return this.price * this.quantity + this.delivery;
+};
+
+
+var sizePrice = [1200, 900, 650]
+var deliverPrices = [0, 300];
+$(document).ready(function () {
+  $('form#myForm').submit(function (event) {
+      event.preventDefault();
+      var pizzaType = $('#type').val();
+
+      var pizzaSize = parseInt($('#size').val());
+
+      var pizzaCrust = $('#crust').val();
+
+      var pizzaTop = $('#top').val();
+
+      var pizzaQty = parseInt($('#qty').val());
+
+      var pizzaPick = parseInt($('#pick').val());
+
+
+      var price = sizePrice[pizzaSize - 1];
+
+
+      var DeliveryCost = deliverPrices[pizzaPick - 1];
+
+
+
+      newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+      newTotal = new Total(price, pizzaQty, DeliveryCost);
+      if (pizzaPick===1){
+      alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+      alert("your bill is: " + newTotal.finalTotal() + ".You will receive your pizza in the next few minutes");
+      }else{
+          if(pizzaPick===2){
+              prompt("Enter your location");
+              alert("Your order has been received and it will be delivered. Continue to see your order details");
+              alert("Your order is: " + newOrder.fullOrder() + "continue");
+              alert("your bill is: " + newTotal.finalTotal() + ".You pizza will be delevered in the next 30minutes");
+          }
+      }
+
+  });
+})
